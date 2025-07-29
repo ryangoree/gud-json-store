aff9c1e16f69818f3c0017d57344570e0ef48228
Ryan Goree <goree.ryan@gmail.com>
2025-07-28 19:03:53 -0500

    make import patterns consistent

--- src/utils/getOSConfigDir.ts
+++ src/utils/getOSConfigDir.ts
@@ -1,5 +1,5 @@
-import os from "node:os";
-import path from "node:path";
+import { homedir, platform } from "node:os";
+import { join } from "node:path";
 
 /**
  * Get the path to an app specific config directory based on operating system
@@ -9,28 +9,27 @@ import path from "node:path";
  * is being created.
  */
 export function getOSConfigDir(projectName: string): string {
-  const homeDir = os.homedir();
-  const platform = os.platform();
+  const home = homedir();
+  const platformId = platform();
+  let base: string;
 
-  let configDir: string;
-
-  if (platform === "win32") {
+  if (platformId === "win32") {
     // Windows
     // https://learn.microsoft.com/en-us/windows/apps/design/app-settings/store-and-retrieve-app-data
     // https://learn.microsoft.com/en-us/windows/uwp/get-started/fileio-learning-track#app-folders
     // https://learn.microsoft.com/en-us/windows/deployment/usmt/usmt-recognized-environment-variables#variables-that-are-recognized-only-in-the-user-context
-    configDir = process.env.APPDATA || path.join(homeDir, "AppData", "Roaming");
-  } else if (platform === "darwin") {
+    base = process.env.APPDATA || join(home, "AppData", "Roaming");
+  } else if (platformId === "darwin") {
     // macOS
     // https://developer.apple.com/library/archive/documentation/FileManagement/Conceptual/FileSystemProgrammingGuide/MacOSXDirectories/MacOSXDirectories.html
     // https://apple.fandom.com/wiki/Application_Support_folder
-    configDir = path.join(homeDir, "Library", "Application Support");
+    base = join(home, "Library", "Application Support");
   } else {
     // Linux and others
     // https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
     // https://how-to.fandom.com/wiki/Guide_to_linux_configuration_files
-    configDir = process.env.XDG_CONFIG_HOME || path.join(homeDir, ".config");
+    base = process.env.XDG_CONFIG_HOME || join(home, ".config");
   }
 
-  return path.join(configDir, `${projectName}`);
+  return join(base, `${projectName}`);
 }
